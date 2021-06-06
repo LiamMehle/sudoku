0e7380365d8df5b6dbdb810fb214b59a8b30366a
wolf <liam@limar.tk>
2021-06-06 22:28:38 +0200

    added doxy comments for validity checking

--- src/sudoku_validity_check.c
+++ src/sudoku_validity_check.c
@@ -1,5 +1,14 @@
 #include "sudoku_validity_check.h"
 
+
+/**
+ * @brief preveri validnost v vrstici
+ * Preveri ali število se pojavi v isti vrstici enkrat ali več
+ * @param sudoku Board tip, ki zapisuje stanje igre ob vnosu vrednosti
+ * @param row indeks vrstice, v katero spada število
+ * @param liczba število za vpisati v vrstico
+ * @return 0, če se število ne pojavi v vrstici, ne-0 če se.
+ */
 /* Function checks, if in given row the number already exists */
 int check_row(Board sudoku, int row, int liczba)
 {
@@ -12,6 +21,14 @@ int check_row(Board sudoku, int row, int liczba)
     return 0;
 }
 
+/**
+ * @brief preveri validnost v stolpcu
+ * Preveri ali število se pojavi v istem stolpcu enkrat ali več
+ * @param sudoku Board tip, ki zapisuje stanje igre ob vnosu vrednosti
+ * @param row indeks stolpca, v katero spada število
+ * @param liczba število za vpisati v stolpec
+ * @return 0, če se število ne pojavi v stolpcu, ne-0 če se.
+ */
 /* Function checks, if in given column the number already exists */
 int check_collum(Board sudoku, int column, int liczba)
 {
@@ -24,8 +41,17 @@ int check_collum(Board sudoku, int column, int liczba)
     return 0;
 }
 
+/**
+ * @brief preveri validnost v oknu
+ * Preveri ali število se pojavi v istem oknu enkrat ali več
+ * @param sudoku Board tip, ki zapisuje stanje igre ob vnosu vrednosti
+ * @param collum x koordinata celice, v katerem bo vpisano število
+ * @param row y koordinata celice, v katerem bo vpisano število
+ * @param liczba število za vpisati v celico
+ * @return 0, če se število ne pojavi v vrstici, ne-0 če se.
+ */
 /* Function checks, if in given grid the number already exists */
-int check_grid(Board sudoku, int row, int column, int liczba)
+int check_grid(Board sudoku, int collum, int row, int liczba)
 {
     row = (row/3)*3;
     column = (column/3)*3;
